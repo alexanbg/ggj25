002a0c40e7055e31ae6232e6508ae49576b607f1
alexanbg <alexanbg@abv.bg>
2025-01-25 12:33:48 +0200

    Added movement to outtter sphere

--- Assets/Scripts/OutterSphereMovement.cs
+++ Assets/Scripts/OutterSphereMovement.cs
@@ -1,14 +1,14 @@
 using UnityEngine;
 
-public class SphereMovement : MonoBehaviour
+public class OutterSphereMovement : MonoBehaviour
 {
     [SerializeField]
     private float rotationSpeed = 1000;
 
-    
+
     public void Update()
     {
         //rb.AddRelativeTorque(keyValues.x * rotationSpeed * Time.deltaTime, 0, keyValues.y * rotationSpeed * Time.deltaTime);
-        transform.Rotate(new Vector3(Input.GetAxisRaw("Vertical") * rotationSpeed * Time.deltaTime, -Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime, 0),Space.World);
+        transform.Rotate(new Vector3(Input.GetAxisRaw("Debug Vertical") * rotationSpeed * Time.deltaTime, -Input.GetAxisRaw("Debug Horizontal") * rotationSpeed * Time.deltaTime, 0), Space.World);
     }
 }
