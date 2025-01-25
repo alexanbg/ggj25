d6b3f3ac292d33a48ce7792be0cee766839647b8
alexanbg <alexanbg@abv.bg>
2025-01-25 19:40:59 +0200

    Added pick-ups

---
+++ Assets/Scripts/ScorePickUp.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ScorePickUp : MonoBehaviour
+{
+    [SerializeField]
+    private float innerCircleRadius;
+    [SerializeField]
+    private float outterCircleradius;
+    [SerializeField]
+    private float minTeleportDistance;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log($"Collided with {LayerMask.LayerToName(collision.collider.gameObject.layer)}");
+        if (LayerMask.LayerToName(collision.collider.gameObject.layer)=="Ball")
+        {
+            collision.collider.transform.GetComponent<ScoringSystem>().UpdateScore(200);
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        Vector3 newPosition = new Vector3();
+        do
+        {
+            newPosition = new Vector3(
+            Random.Range(-outterCircleradius, outterCircleradius),
+            Random.Range(-outterCircleradius, outterCircleradius),
+            Random.Range(-outterCircleradius, outterCircleradius)
+            );
+        } while (Vector3.Distance(transform.position, newPosition) < minTeleportDistance || Vector3.Distance(newPosition, Vector3.zero) < innerCircleRadius);
+
+        transform.position = newPosition;
+    }
+}
